3bc71e71d02135755f146ddf948a8a1c68e195e3
john98santos <johnsousasantos98@gmail.com>
2020-10-16 14:08:46 -0300

    Criando Sockets e metodos de conexão

--- WPF - Chat/WPF - Chat/Dialogs/DialogYesNo/DialogYesNoViewModel.cs
+++ WPF - Chat/WPF - Chat/Dialogs/DialogYesNo/DialogYesNoViewModel.cs
@@ -3,43 +3,109 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
 using System.Windows;
 using System.Windows.Input;
 using WPF___Chat.Dialogs.DialogService;
+using WPF___Chat.Models;
 using static WPF___Chat.Dialogs.DialogService.DialogResultEnum;
+using System.Windows.Data;
 
 namespace WPF___Chat.Dialogs.DialogYesNo
 {
     class DialogYesNoViewModel : DialogViewModelBase
     {
-        private ICommand yesCommand = null;
-        public ICommand YesCommand
-        {
-            get { return yesCommand;  }
-            set { yesCommand = value; }
-        }
+        private string status = "Offline";
+        private Connection connection = new Connection();
 
         private ICommand noCommand = null;
         public ICommand NoCommand
         {
-            get { return noCommand;  }
+            get { return noCommand; }
             set { noCommand = value; }
         }
 
         public DialogYesNoViewModel()
         {
-            this.yesCommand = new RelayCommand(OnYesClicked);
+            StartConnection();
             this.noCommand = new RelayCommand(OnNoClicked);
         }
-
-        private void OnYesClicked(object parameter)
-        {
-            this.CloseDialogWithResult(parameter as Window, DialogResult.Yes);
-        } 
 
         private void OnNoClicked(object parameter)
         {
             this.CloseDialogWithResult(parameter as Window, DialogResult.No);
+        }
+
+        private ICommand _btn_Send;
+
+        public ICommand Btn_Send
+        {
+            get
+            {
+                if (_btn_Send == null)
+                {
+                    _btn_Send = new RelayCommand(param => this.Send());
+                }
+                return _btn_Send;
+            }
+        }
+
+        private string historicoChat;
+
+        public string chatBox
+        {
+            get { return historicoChat; }
+            set { historicoChat = value; }
+        }
+
+        private string msgSend;
+
+        public string sendBox
+        {
+            get { return msgSend; }
+            set { msgSend = value; }
+        }
+
+        public void StartConnection()
+        {
+            try {
+                connection.Start();
+                status = "Online";
+            }
+            catch
+            {
+                status = "Offline";
+            }
+        }
+
+        public void Send()
+        {
+            connection.SendMessage(msgSend);
+
+            UpdateChat("[You]: " + msgSend);
+
+            
+        }
+
+        public void Receive()
+        {
+            string msgReceived;
+            msgReceived = connection.ReceiveMessage();
+
+            UpdateChat("[Stranger]: " + msgReceived);
+        }
+
+        public void UpdateChat(string message)
+        {
+            if (historicoChat == "")
+            {
+                historicoChat = message;
+            }
+            else {
+                historicoChat = historicoChat + "\n\n" + message;
+            }
+            //chatBox.text;
+
         }
     }
 }
---
+++ WPF - Chat/WPF - Chat/Models/Connection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace WPF___Chat.Models
+{
+    class Connection
+    {
+        private Socket tcpSocket = null;
+
+        private IPEndPoint clientEndpoint = new IPEndPoint(IPAddress.Parse("192.168.10.10"), 30789);
+        private IPEndPoint listenerEndpoint = new IPEndPoint(IPAddress.Parse("192.168.10.10"), 30789);
+
+        private TcpClient tcpClient = null;
+        private TcpListener tcpListener = null;
+
+        public void Start()
+        {
+            tcpSocket = Client();
+
+            if (tcpSocket == null)
+            {
+                tcpSocket = Listener();
+            }
+        }
+
+        public void SendMessage(string chat)
+        {
+            byte[] message = Encoding.ASCII.GetBytes(chat);
+            tcpSocket.Send(message);
+            Console.WriteLine("Source Send: " + Encoding.UTF8.GetString(message));
+        }
+
+        public string ReceiveMessage()
+        {
+            byte[] message = new byte[30];
+            tcpSocket.Receive(message);
+            Console.WriteLine("Dest Receive: " + Encoding.UTF8.GetString(message));
+
+            return Encoding.UTF8.GetString(message);
+        }
+
+        public Socket Client()
+        {
+            try
+            {
+
+                tcpClient = new TcpClient();
+                tcpClient.Connect(listenerEndpoint.Address, 30789);
+
+                tcpSocket = tcpClient.Client;
+
+                return tcpSocket;
+            }
+
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        public Socket Listener()
+        {
+            tcpListener = new TcpListener(clientEndpoint);
+            tcpListener.Start();
+            tcpSocket = tcpListener.AcceptSocket();
+
+            return tcpSocket;
+            // tcpListener.Stop();
+        }
+    }
+}
